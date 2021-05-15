dde767eb5f2a096275289812f843ab53779f2422
rajdeep345 <rajdeep345.mukherjee@yahoo.com>
2021-05-15 07:36:00 +0530

    Added script for BERT

---
+++ code/BertModel/run_script.sh
@@ -0,0 +1,32 @@
+python main.py --task triplet --device cuda:1 --mode train --dataset lap14 > train_lap14_run1.out
+python main.py --task triplet --device cuda:1 --mode test --dataset lap14 > lap14_run1.out
+python main.py --task triplet --device cuda:1 --mode train --dataset res14 > train_res14_run1.out
+python main.py --task triplet --device cuda:1 --mode test --dataset res14 > res14_run1.out
+python main.py --task triplet --device cuda:1 --mode train --dataset res15 > train_res15_run1.out
+python main.py --task triplet --device cuda:1 --mode test --dataset res15 > res15_run1.out
+python main.py --task triplet --device cuda:1 --mode train --dataset res16 > train_res16_run1.out
+python main.py --task triplet --device cuda:1 --mode test --dataset res16 > res16_run1.out
+python main.py --task triplet --device cuda:1 --mode train --dataset resall > train_resall_run1.out
+python main.py --task triplet --device cuda:1 --mode test --dataset resall > resall_run1.out
+
+python main.py --task triplet --device cuda:1 --mode train --dataset lap14 > train_lap14_run2.out
+python main.py --task triplet --device cuda:1 --mode test --dataset lap14 > lap14_run2.out
+python main.py --task triplet --device cuda:1 --mode train --dataset res14 > train_res14_run2.out
+python main.py --task triplet --device cuda:1 --mode test --dataset res14 > res14_run2.out
+python main.py --task triplet --device cuda:1 --mode train --dataset res15 > train_res15_run2.out
+python main.py --task triplet --device cuda:1 --mode test --dataset res15 > res15_run2.out
+python main.py --task triplet --device cuda:1 --mode train --dataset res16 > train_res16_run2.out
+python main.py --task triplet --device cuda:1 --mode test --dataset res16 > res16_run2.out
+python main.py --task triplet --device cuda:1 --mode train --dataset resall > train_resall_run2.out
+python main.py --task triplet --device cuda:1 --mode test --dataset resall > resall_run2.out
+
+python main.py --task triplet --device cuda:1 --mode train --dataset lap14 > train_lap14_run3.out
+python main.py --task triplet --device cuda:1 --mode test --dataset lap14 > lap14_run3.out
+python main.py --task triplet --device cuda:1 --mode train --dataset res14 > train_res14_run3.out
+python main.py --task triplet --device cuda:1 --mode test --dataset res14 > res14_run3.out
+python main.py --task triplet --device cuda:1 --mode train --dataset res15 > train_res15_run3.out
+python main.py --task triplet --device cuda:1 --mode test --dataset res15 > res15_run3.out
+python main.py --task triplet --device cuda:1 --mode train --dataset res16 > train_res16_run3.out
+python main.py --task triplet --device cuda:1 --mode test --dataset res16 > res16_run3.out
+python main.py --task triplet --device cuda:1 --mode train --dataset resall > train_resall_run3.out
+python main.py --task triplet --device cuda:1 --mode test --dataset resall > resall_run3.out
